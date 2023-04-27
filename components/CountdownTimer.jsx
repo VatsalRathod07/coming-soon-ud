415ca1b1d989b7238e903bb01ba0bb7459f431dd
VatsalRathod07 <vatsalrathod2005@gmail.com>
2023-04-27 14:39:29 +0530

    removed some unused CSS & JavaScript

--- components/CountdownTimer.jsx
+++ components/CountdownTimer.jsx
@@ -1,30 +1,3 @@
-// import React, { useState, useEffect } from "react";
-
-// const CountdownTimer = () => {
-//   const [timeLeft, setTimeLeft] = useState("");
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       const end = new Date("2023-05-05T07:00:00Z"); // replace with your end time
-//       const now = new Date();
-//       const diff = end.getTime() - now.getTime();
-
-//       const days = Math.floor(diff / (1000 * 60 * 60 * 24));
-//       const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-//       const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
-//       const seconds = Math.floor((diff % (1000 * 60)) / 1000);
-
-//       setTimeLeft(`${days}d  ${hours}h  ${minutes}m  ${seconds}s`);
-//     }, 1000);
-
-//     return () => clearInterval(interval);
-//   }, []);
-
-//   return <div className="text-2xl font-semibold uppercase">{timeLeft}</div>;
-// };
-
-// export default CountdownTimer;
-
 import React, { useState, useEffect } from "react";
 
 const Timer = () => {
@@ -58,12 +31,7 @@ const Timer = () => {
 
   return (
     <div className="flex justify-center items-center h-30">
-      <div
-        // className={`${
-        //   isBlack ? "bg-black  text-white" : "bg-white text-black"
-        // } font-bold text-4xl p-4 rounded-md transform duration-500 ease-in-out`}
-        className="font-bold text-4xl text-black"
-      >
+      <div className="font-bold text-4xl text-black" >
         {timeLeft}
       </div>
     </div>
